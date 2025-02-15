4c17be4254598b1da99cb7013ec59ff056ce60d3
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2025-02-15 21:00:09 +0000

    test update

--- dist/natives/queue.js
+++ dist/natives/queue.js
@@ -16,7 +16,6 @@ exports.default = new forgescript_1.NativeFunction({
         const player = kazagumo.getPlayer((guild.id ?? ctx.guild.id));
         if (!player)
             return this.customError("No player found!");
-        player.queue.clear();
-        return this.success();
+        return this.successJSON(player.queue.map(x => x.getRaw));
     }
 });
