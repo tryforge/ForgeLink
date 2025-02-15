384dbae5e55aaba1313f3a0255eb2fbde0be1829
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2025-02-15 18:27:46 +0000

    build

--- dist/classes/managers/KazagumoCommandManager.js
+++ dist/classes/managers/KazagumoCommandManager.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.KazagumoCommandManager = void 0;
+exports.KazagumoCommandManager = exports.handlerName = void 0;
 const forgescript_1 = require("@tryforge/forgescript");
+exports.handlerName = "ForgeLink";
 class KazagumoCommandManager extends forgescript_1.BaseCommandManager {
     handlerName = 'kazagumoCommands';
 }
