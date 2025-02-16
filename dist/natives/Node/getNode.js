ae7bb2c7d7849bf8fb60ff38ea2e1466f620e4a0
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2025-02-16 18:52:54 +0000

    test

--- dist/natives/Node/getNode.js
+++ dist/natives/Node/getNode.js
@@ -5,7 +5,7 @@ const ForgeLink_1 = require("../../classes/structures/ForgeLink");
 exports.default = new forgescript_1.NativeFunction({
     name: '$getNodes',
     description: 'Gets Player Nodes',
-    brackets: true,
+    brackets: false,
     unwrap: true,
     args: [
         forgescript_1.Arg.requiredGuild('Guild ID', 'The ID of the guild of the player to.'),
@@ -13,6 +13,11 @@ exports.default = new forgescript_1.NativeFunction({
     output: forgescript_1.ArgType.Boolean,
     execute: async function (ctx, [guild = ctx.guild]) {
         const kazagumo = ctx.client.getExtension(ForgeLink_1.ForgeLink, true).kazagumo;
-        return this.success(kazagumo.shoukaku.nodes);
+        const nodes = Array.from(kazagumo.shoukaku.nodes.values()).map(node => ({
+            name: node.name,
+            auth: node.info.lavaplayer,
+            url: node.ws.url
+        }));
+        return this.success();
     }
 });
--- src/natives/Node/getNode.ts
+++ src/natives/Node/getNode.ts
@@ -5,7 +5,7 @@ import { ForgeLink } from '@structures/ForgeLink'
 export default new NativeFunction({
     name: '$getNodes',
     description: 'Gets Player Nodes',
-    brackets: true,
+    brackets: false,
     unwrap: true,
     args: [
         Arg.requiredGuild('Guild ID', 'The ID of the guild of the player to.'),
@@ -14,8 +14,13 @@ export default new NativeFunction({
     execute: async function(ctx, [guild = ctx.guild]) {
         const kazagumo = ctx.client.getExtension(ForgeLink, true).kazagumo
 
-        
+       const nodes = Array.from(kazagumo.shoukaku.nodes.values()).map(node => ({
+        name: node.name,
+        auth: node.info.lavaplayer,
+        url: node.ws.url
 
-        return this.success(kazagumo.shoukaku.nodes)
+       }))
+
+        return this.success()
     }
 })
